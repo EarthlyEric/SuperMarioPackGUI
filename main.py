22fd813120a703da330d07014aa5361e189d1420
EarthlyEric6 <60834421+EarthlyEric@users.noreply.github.com>
2021-07-01 21:38:06 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from  tkinter import*
-import lib,webbrowser,json,button_lib as bt,lang_loader as lang
+import lib,webbrowser,json,button_lib as bt,lang_loader as lang,winsound
+from tkinter import ttk
 
 mainwin = Tk() # 主視窗建立
 
@@ -22,15 +23,38 @@ b_run_smb2_canvas = canvas_mainwin.create_window(605,205,anchor="nw",window = b_
 b_run_sm3 = Button(text=f"{lang.b_run_smb3_lang}",background="red",relief=RAISED,width=7,height=2,command=bt.b_run_smb3_run)
 b_run_sm3_canvas = canvas_mainwin.create_window(1030,205,anchor="nw",window = b_run_sm3)
 
-def b_run_settings():
+def b_run_settings_os():
     settings = Toplevel(mainwin)
+    settings.title("Settings")
     settings.geometry("300x450")
+
+    with open("config\config.json") as lang_get:
+            lang_get_load_temp = json.load(lang_get)
+            lang_get_load = lang_get_load_temp["lang"]
+
     settings.resizable(False,False)
-    lang_button = Button(settings, text = "New Window button")
-    lang_button.pack
+    lang_combo = ttk.Combobox(settings,values=["English","繁體中文"])
+
+    lang_combo.grid(column=0, row=1)#設語言id
+    if lang_get_load == "zh_tw":
+        pass
+    elif lang_get_load =="en_us":
+        pass
+    lang_combo.current(0)#預設語言id
+    print(lang_combo.get())#選擇語言結果名
+    print(lang_combo.current())#選擇語言結果id
+    lang_switch = lang_combo.current()
+    
+    
+    if lang_switch == 0:
+        with open("config\config.json") as lang_change:
+            lang_change_load = lang_change["lang"]
+    elif lang_switch == 1:
+        print("完成")
+             
 
 b_run_settings_img = PhotoImage(file="res\Settings_icon.png")
-b_run_settings = Button(background="white",image=b_run_settings_img,relief=RAISED,command=b_run_settings)
+b_run_settings = Button(background="white",image=b_run_settings_img,relief=RAISED,command=b_run_settings_os)
 b_run_setting_canvas = canvas_mainwin.create_window(1248,46,anchor="nw",window=b_run_settings)
 
 b_run_exit_img = PhotoImage(file="res\Exit_icon.png")
